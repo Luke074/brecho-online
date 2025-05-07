46c83fd6264b7e97449dc4a4b0f507ca75587b25
Lucas Alves <68557804+Luke074@users.noreply.github.com>
2025-05-06 21:37:39 -0300

    fix: ajustando estrutura e rotas de get e post

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 import sqlite3
 import os
 
+from interface.pessoa import PessoaCreate
+from interface.produto import ProdutoCreate  # Corrected import path
+
 app = FastAPI()
 
 os.makedirs('db', exist_ok=True)
@@ -23,6 +26,7 @@ def create_structure_database():
             id_pessoa INTEGER PRIMARY KEY AUTOINCREMENT,
             nome VARCHAR(100),
             email VARCHAR(100),
+            senha VARCHAR(100),
             celular VARCHAR(20),
             endereco VARCHAR(255),
             documento VARCHAR(20)
@@ -92,3 +96,66 @@ def create_structure_database():
     
     connection.commit()
     connection.close()
+    
+    
+
+    
+@app.post("/login")
+async def login(usuario: str, senha: str):
+    # Aqui você pode implementar a lógica de autenticação
+    
+    # Por enquanto, vamos apenas retornar uma mensagem de sucesso
+    return {"message": "Login bem-sucedido!"}
+    
+@app.post("/pessoas")
+async def adicionar_pessoa(pessoa: PessoaCreate):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO pessoas (nome, email, celular, endereco, documento, senha)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (
+        pessoa.nome,
+        pessoa.email,
+        pessoa.celular,
+        pessoa.endereco,
+        pessoa.documento,
+        pessoa.senha
+    ))
+    connection.commit()
+    pessoa_id = cursor.lastrowid
+    connection.close()
+    return {"id_pessoa": pessoa_id, "mensagem": "Pessoa cadastrada com sucesso"}
+
+@app.get("/produtos")
+async def listar_produtos():
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM produtos")
+    colunas = [desc[0] for desc in cursor.description]
+    produtos = [dict(zip(colunas, linha)) for linha in cursor.fetchall()]
+
+    connection.close()
+    return {"produtos": produtos}
+
+@app.post("/produtos")
+async def adicionar_produto(produto: ProdutoCreate):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO produtos (id_doacao, id_categoria, descricao, marca, tamanho, preco)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (
+        produto.id_doacao,
+        produto.id_categoria,
+        produto.descricao,
+        produto.marca,
+        produto.tamanho,
+        produto.preco
+    ))
+    connection.commit()
+    produto_id = cursor.lastrowid
+    connection.close()
+    return {"id_produto": produto_id, "mensagem": "Produto cadastrado com sucesso"}
+
